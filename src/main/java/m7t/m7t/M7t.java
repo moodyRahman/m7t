2b880f37ef1c5253c80666ddee16e574d1d50291
Moody Rahman <moody.byte@gmail.com>
2024-01-06 13:19:23 -0500

    walk listener

--- src/main/java/m7t/m7t/M7t.java
+++ src/main/java/m7t/m7t/M7t.java
@@ -1,17 +1,21 @@
 package m7t.m7t;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class M7t extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        // Plugin startup
+        Bukkit.getLogger().info("Hello there! Thank you for using m7t, a moody rahman project :)");
+        getServer().getPluginManager().registerEvents(new WalkListener(), this);
     }
+
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        Bukkit.getLogger().info("Goobnight");
     }
 }
---
+++ src/main/java/m7t/m7t/WalkListener.java
@@ -0,0 +1,20 @@
+package m7t.m7t;
+
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerMoveEvent;
+
+public class WalkListener implements Listener {
+
+    @EventHandler
+    public void onPlayerMove(PlayerMoveEvent event) {
+        Bukkit.getLogger().info("detected some movement from " + event.getPlayer().getName() + " "
+                + event.getPlayer().getLocation().getX() + ", "
+                + event.getPlayer().getLocation().getY() + ", "
+                + event.getPlayer().getLocation().getZ() + ", "
+        );
+
+        event.getPlayer().setLevel((int)event.getPlayer().getLocation().getY());
+    }
+}
