dc8e6e77e5a6e0f4d2d1faa14c8de223eca11eaf
Moody Rahman <moody.byte@gmail.com>
2024-01-06 16:30:00 -0500

    enable javalin server on plugin init

--- src/main/java/m7t/m7t/M7t.java
+++ src/main/java/m7t/m7t/M7t.java
@@ -2,16 +2,25 @@ package m7t.m7t;
 
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+import io.javalin.Javalin;
 
 public final class M7t extends JavaPlugin {
 
     @Override
     public void onEnable() {
         // Plugin startup
-        Bukkit.getLogger().info("Hello there! Thank you for using m7t, a moody rahman project :)");
+        Bukkit.getLogger()
+                .info("Hello there! Thank you for using m7t, a moody rahman project :), made proudle in VSCode");
         getServer().getPluginManager().registerEvents(new WalkListener(), this);
-    }
 
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        Thread.currentThread().setContextClassLoader(this.getClassLoader());
+        Javalin app = Javalin.create().start(8080);
+        Thread.currentThread().setContextClassLoader(classLoader);
+        app.get("/", ctx -> ctx.result("Hello World!"));
+        getLogger().info("JavalinPlugin is enabled");
+
+    }
 
     @Override
     public void onDisable() {
