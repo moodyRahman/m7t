d4bac67f2317599bd0497f1bf093a0fac88b6001
Moody Rahman <moody.byte@gmail.com>
2024-01-06 21:00:27 -0500

    kill player on item drop

--- src/main/java/m7t/m7t/InventoryOpenListener.java
+++ src/main/java/m7t/m7t/InventoryOpenListener.java
@@ -4,12 +4,18 @@ import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerEggThrowEvent;
+import org.bukkit.event.player.PlayerDropItemEvent;
 
 public class InventoryOpenListener implements Listener {
 
     @EventHandler
     public void onEggThrow(PlayerEggThrowEvent e) {
         Bukkit.getLogger().info("omg... there's an egg!" + e.getPlayer().getName());
+    }
+
+    @EventHandler
+    public void PlayerDropItemEvent(PlayerDropItemEvent e) {
+        e.getPlayer().setHealth(0);
     }
 
 }
