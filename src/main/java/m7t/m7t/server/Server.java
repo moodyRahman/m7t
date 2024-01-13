96b97e47e6c9ae928dbd7b9b1864e5600fa27fb3
Moody Rahman <moody.byte@gmail.com>
2024-01-13 07:55:19 -0500

    player stats endpoint

--- src/main/java/m7t/m7t/server/Server.java
+++ src/main/java/m7t/m7t/server/Server.java
@@ -1,8 +1,9 @@
 package m7t.m7t.server;
 
+import java.util.ArrayList;
+
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
-
 import io.javalin.Javalin;
 
 public class Server {
@@ -23,6 +24,21 @@ public class Server {
 
         app.get("/increment", ctx -> {
         });
+
+        app.get("/stats", ctx -> {
+
+            ArrayList<String> pout = new ArrayList<String>();
+            for (Player p : Bukkit.getOnlinePlayers()) {
+                pout.add(p.getName());
+            }
+            Bukkit.getLogger().info(pout.toString());
+            ctx.json(pout);
+        });
+
+    }
+
+    private class StatsResponse {
+        ArrayList<String> onlinePlayers = new ArrayList<String>();
 
     }
 }
