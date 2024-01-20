de2d2b102e2949b02c1e2d823a8130e5e38d0ec6
Moody Rahman <moody.byte@gmail.com>
2024-01-20 15:29:43 -0500

    remain command switch statement

--- src/main/java/m7t/m7t/commands/RemainCommand.java
+++ src/main/java/m7t/m7t/commands/RemainCommand.java
@@ -1,14 +1,31 @@
 package m7t.m7t.commands;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+
+import m7t.m7t.db.Db;
 
 public class RemainCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        sender.sendMessage("pong");
+
+        String subcommand = args[0];
+
+        switch (subcommand) {
+            case "remain":
+                int t = Db.getRemainingTime();
+                sender.sendMessage(Integer.toString(t) + " minutes remaining");
+                break;
+
+            case "add":
+                Db.increment(60);
+                break;
+
+        }
+
         return true;
     }
 
